633c1c0843fb9dedb2304dbf8d2307898d7a7cfe
fd1az <fdiaz.dev@gmail.com>
2022-07-26 23:21:58 -0300

    middleware cors

--- pages/api/post.js
+++ pages/api/post.js
@@ -1,7 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { createPost, getAllPosts } from "../../repo/post.js";
+import Cors from "cors";
+const cors = Cors({
+  methods: ["POST", "GET", "HEAD"],
+});
 
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
 export default async function handler(req, res) {
+  await runMiddleware(req, res, cors);
   switch (req.method) {
     case "GET":
       const posts = await getAllPosts();
